be08c341f28438ad4c03d7eea44ceb93118584bf
huanghao <1063462242@qq.com>
2024-06-28 09:30:57 +0800

    代码规范设置

---
+++ vue3-typescript-demo/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup() {
+    const message = ref<string>('')
+    const reset = () => {
+      message.value = ''
+    }
+    return {
+      reset
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
